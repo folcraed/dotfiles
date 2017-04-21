71d15bddc00bc9e48e6cf0a9de3eedbd5c13aa66
folcraed <robttheb@gmail.com>
2017-04-20 21:18:52 -0400

    changed weather location code in lain widget

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -120,7 +120,7 @@ lain.widget.calendar({
         fg = "#81a2be"},
 })
 myweather   = lain.widget.weather({
-    city_id = 5666639,
+    city_id = 5202855,
     units   = "imperial",
     settings = function()
         descr = weather_now["weather"][1]["description"]:lower()
