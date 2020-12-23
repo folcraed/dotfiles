d0470bd7ffa0266407e87fd180420070d24103d0
folcred <robttheb@gmail.com>
2020-12-22 19:00:56 -0700

    Added pulseaudio startup
    For some reason since the last update, pulseaudio isn't starting, I have to manually start it. Added it to configs until I figure out what's wrong.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -673,6 +673,7 @@ end
 run_once("gnome-keyring-daemon -s")
 run_once("setxkbmap -option caps:escape")
 run_once("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+run_once("start-pulseaudio-x11")
 run_once("ssh-agent")
 run_once("picom")
 run_once("clipit")
