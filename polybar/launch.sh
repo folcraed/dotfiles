e982df2993480f8821967947948381df0482203d
folcred <robttheb@gmail.com>
2020-12-26 13:47:46 -0700

    Changes to work with multi-monitors
    Needed to make some changes to all these files to get them to play
    well together with dual monitors. Took a little research but did
    manage to get it all working. Pretty happy with the results too.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,4 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-for m in ${polybar --list-monitors | cut -d ":" -f1}; do
-  MONITOR=$m polybar --reload bspwmbar &
-done
+polybar bspwmbar & polybar bar2
