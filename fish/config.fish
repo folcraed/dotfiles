e649578315d599823c96fd36212d2f69788c2a43
folcred <robttheb@gmail.com>
2021-03-28 16:54:13 -0600

    got rid of python logging in Temp

--- fish/config.fish
+++ fish/config.fish
@@ -13,14 +13,14 @@ function bu
 end
 
 function bu2
-    rsync -aiv --delete /home/rob/Dropbox/Genealogy/ /mnt/rob/Dropbox/Genealogy &&
-    rsync -aiv --delete /home/rob/Dropbox/Mindmaps/ /mnt/rob/Dropbox/Mindmaps &&
-    rsync -aiv --delete /home/rob/Dropbox/Notes/ /mnt/rob/Dropbox/Notes &&
-    rsync -aiv --delete /home/rob/Dropbox/Docs/ /mnt/rob/Dropbox/Docs
+    rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Genealogy/ /mnt/rob/Dropbox/Genealogy &&
+    rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Mindmaps/ /mnt/rob/Dropbox/Mindmaps &&
+    rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Notes/ /mnt/rob/Dropbox/Notes &&
+    rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Docs/ /mnt/rob/Dropbox/Docs
 end
 
 function cdd
-	cd ~/dotfiles
+	cd ~/.config/dotfiles
 end
 
 function cdt
