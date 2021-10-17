953845fea253984dce342db7d4f66b3de1c3218e
folcred <robttheb@gmail.com>
2021-10-16 19:24:26 -0600

    Added notebook backup, thought I already head it
    I pulled from GitHub, but must not have pushed first, at least
    that's what I think, I didn't get the update I'd done on Plasma
    disk, so must not have.

--- fish/config.fish
+++ fish/config.fish
@@ -16,11 +16,13 @@ function bu2
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Genealogy/ /mnt/rob/Dropbox/Genealogy &&
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Mindmaps/ /mnt/rob/Dropbox/Mindmaps &&
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Notes/ /mnt/rob/Dropbox/Notes &&
+    rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Notebook/ /mnt/rob/Dropbox/Notebook &&
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Docs/ /mnt/rob/Dropbox/Docs &&
     rsync -aiv --delete --exclude='.comments/' ~/.googleearth/myplaces.kml /mnt/rob/.googleearth
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Genealogy/ /run/media/rob/Dropbox/Genealogy &&
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Mindmaps/ /run/media/rob/Dropbox/Mindmaps &&
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Notes/ /run/media/rob/Dropbox/Notes &&
+    rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Notebook/ /run/media/rob/Dropbox/Notebook &&
     rsync -aiv --delete --exclude='.comments/' /home/rob/Dropbox/Docs/ /run/media/rob/Dropbox/Docs
     rsync -aiv --delete --exclude='.comments/' ~/.googleearth/myplaces.kml /run/media/rob/.googleearth
 end
