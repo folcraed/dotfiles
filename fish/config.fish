492974ffc6743b79987f5d181805157cd6ee02d7
folcred <robttheb@gmail.com>
2021-10-30 15:28:24 -0600

    Added some configs and plugins to micro editor
    Going to give Micro a good spin, it seems a simple but powerful
    little editor that's a lot easier to use than most. I really don't
    need a complicated editor for simple things.

--- fish/config.fish
+++ fish/config.fish
@@ -103,6 +103,18 @@ function extract --description "Expand or extract bundled & compressed files"
   end
 end
 
+function fzf
+    set -l epoch (date "+%s")
+    set -l file_path $TMPDIR/fzf-$epoch.result
+    command fzf $argv >$file_path
+    if test $status -eq 0 -a -s $file_path
+        cat $file_path
+    end
+    if test -e $file_path
+        rm $file_path
+    end
+end
+
 function fzb --description "Shows a preview of files being searched"
     fzf -e --preview="bat --color always {}"
 end
