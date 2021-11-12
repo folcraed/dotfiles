066d2e931a3620fe3715736c505f0c096c8ba00b
folcred <robttheb@gmail.com>
2021-11-11 20:36:35 -0700

    Added a plugin to enable fzf searching within fish
    found a plugin that enables fzf searchs in fish, so installed it
    and gave it a try. Seems to work okay. So far.

--- fish/config.fish
+++ fish/config.fish
@@ -105,26 +105,6 @@ function extract --description "Expand or extract bundled & compressed files"
   end
 end
 
-function fzf
-    set -l epoch (date "+%s")
-    set -l file_path $TMPDIR/fzf-$epoch.result
-    command fzf $argv >$file_path
-    if test $status -eq 0 -a -s $file_path
-        cat $file_path
-    end
-    if test -e $file_path
-        rm $file_path
-    end
-end
-
-function fzb --description "Shows a preview of files being searched"
-    fzf -e --preview="bat --color always {}"
-end
-
-function fzk
-    fzf_key_bindings
-end
-
 function gcam
 	git commit -a
 end
