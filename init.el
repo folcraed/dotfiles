31ad84ff4fce9cd1815f2c28921278356da227bf
folcred <robttheb@gmail.com>
2022-05-13 21:41:53 -0600

    Updated package settings for 28.1
    With 28 having elpa and nongnu by default, instead of setq the package
    archives to add melpa, changed it to add-to-list of package archives.

--- init.el
+++ init.el
@@ -25,8 +25,8 @@
 ;;  Set up repositories
 ;; ==============================================
 (require 'package)
-(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 (package-initialize)
 
 (eval-when-compile
