3fe021c546e1f3d117f3d661252c01de4b0de8b8
folcred <robttheb@gmail.com>
2022-07-16 18:58:43 -0600

    Added telescope and friends
    Added telescope now that it's reached some kind of stable state. Set a
    few default key bindings to make it usable for me.

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,13 +16,17 @@ local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'ful1e5/onedark.nvim' -- Theme inspired by Atom
+  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
   use 'vifm/vifm.vim' -- Powerful file manager
   use 'norcalli/nvim-colorizer.lua' -- Shows hex colors in the color
   use 'kyazdani42/nvim-web-devicons' -- Icons for the status line
   use { 'nvim-lualine/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true}
 }
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x',
+         requires = (('nvim-lua/plenary.nvim'))
+}
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
 end)
 
 --**************************
@@ -150,6 +154,29 @@ vim.api.nvim_set_keymap('n', '<leader>vn', [[<cmd>Vifm<CR>]], { noremap = true, 
 vim.api.nvim_set_keymap('n', '<leader>vv', [[<cmd>VsplitVifm<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>vs', [[<cmd>SplitVifm<CR>]], { noremap = true, silent = true })
 
+-- Setup Telescope
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
+
+-- Keys for Telescope
+vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>Telescope find_files<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>Telescope live_grep<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>Telescope buffers<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>Telescope help_tags<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fs', [[<cmd>Telescope current_buffer_fuzzy_find<CR>]], { noremap = true, silent = true })
+
 -- Highlight on yank
 vim.cmd [[
   augroup YankHighlight
@@ -163,19 +190,22 @@ require 'colorizer'.setup()
 
 --Set colorscheme (order is important here)
 require('onedark').setup({
-        comment_style = "italic",
+        style = 'dark',
         transparent = true,
 })
+
+require('onedark').load()
 
 --Set statusbar
 require 'lualine'.setup({
         options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'auto',
         component_separators = { left = ' ', right = ' ' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {},
         always_divide_middle = true,
+        globalstatus = true,
       },
       sections = {
         lualine_a = { 'mode' },
