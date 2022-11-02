2f62f52efe5313958123140fdb9af943703bdf36
folcred <robttheb@gmail.com>
2022-11-02 17:53:05 -0600

    Removed a folder from backups that isn't used anymore
    Moved all the Obsidian files to Emacs, and deleted Obsidian. Deleted
    the folder and removed it from the rsync backups.

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,6 @@ function bu2
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Genealogy/ /run/media/rob/backup/Dropbox/Genealogy
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Mindmaps/ /run/media/rob/backup/Dropbox/Mindmaps
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Notes/ /run/media/rob/backup/Dropbox/Notes
-        rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Notebooks/ /run/media/rob/backup/Dropbox/Notebooks
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Docs/ /run/media/rob/backup/Dropbox/Docs
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Carroll/ /run/media/rob/backup/Dropbox/Carroll
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/.googleearth/myplaces.kml /run/media/rob/backup/.googleearth
