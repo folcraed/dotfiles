3abd3771d3cda18f8fd97e9479e576fe55d2c183
folcred <robttheb@gmail.com>
2023-02-07 10:28:22 -0700

    Removed --delete flag from notes folder
    Better to have backups of ALL notes, even ones I don't think I need
    anymore. Been burned a couple times due to deleting something I
    thought I wouldn't need, then found I did need it.

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,7 @@ end
 function bu2
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Genealogy/ /run/media/rob/backup/Dropbox/Genealogy
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Mindmaps/ /run/media/rob/backup/Dropbox/Mindmaps
-        rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Notes/ /run/media/rob/backup/Dropbox/Notes
+        rsync -aiv --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Notes/ /run/media/rob/backup/Dropbox/Notes
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Notebook/ /run/media/rob/backup/Dropbox/Notebook
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Docs/ /run/media/rob/backup/Dropbox/Docs
         rsync -aiv --delete --exclude='.dtrash' --exclude='.comments/' ~/Dropbox/Carroll/ /run/media/rob/backup/Dropbox/Carroll
@@ -32,10 +32,6 @@ end
 
 function cdn
     cd ~/Dropbox/Notes
-end
-
-function cds
-	cd ~/Dropbox/Settings
 end
 
 function cdg
