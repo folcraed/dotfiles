233f212d09aaa0e968b672cc49603f83c1788ba4
folcred <robttheb@gmail.com>
2023-07-12 21:39:31 -0600

    nvim: Neogit package location change for Packer
    Neogit moved it's git repo to a new one, needed to update the
    address for Packer to pull from.

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,7 +26,7 @@ require('packer').startup(function()
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x',
          requires = (('nvim-lua/plenary.nvim'))
 }
-  use { 'TimUntersberger/neogit',
+  use { 'NeogitOrg/neogit',
          requires = 'nvim-lua/plenary.nvim' }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
 end)
