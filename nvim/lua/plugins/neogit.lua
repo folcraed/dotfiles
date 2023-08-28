f455a6298021c11050a20f7ab3c2565ea81ba965
folcred <robttheb@gmail.com>
2023-08-27 19:34:03 -0600

    nvim: Trying out LazyVim as plugin manager
    A bit over the top for my uses, but we'll give it a go for a while. I tried
    it before, but it was too busy for me. But now that packer is going away,
    I do need to find something to use, so try it again.

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    opts = { disable_commit_confirmation = true },
+    keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" } },
+  },
+}
