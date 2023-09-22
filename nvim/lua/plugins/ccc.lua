ae87c8a7a55e6f1b39a8aba705d076cdc979e107
folcred <robttheb@gmail.com>
2023-09-21 22:06:53 -0600

    nvim: Added color highlight plugin
    Was spoiled having color codes show in the color represented, missed
    having it since changing to Lazy.

---
+++ nvim/lua/plugins/ccc.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "uga-rosa/ccc.nvim",
+    cmd = "CccHighlighterToggle",
+    keys = { { "<leader>tc", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle color hightlight" } },
+  },
+}
