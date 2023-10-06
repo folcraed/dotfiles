f07b373b6de30d33f9fe1a7f2a49911f2dc76654
folcred <robttheb@gmail.com>
2023-10-05 20:50:03 -0600

    nvim: Changed the bindings the colorizer uses

--- nvim/lua/plugins/ccc.lua
+++ nvim/lua/plugins/ccc.lua
@@ -2,6 +2,6 @@ return {
   {
     "uga-rosa/ccc.nvim",
     cmd = "CccHighlighterToggle",
-    keys = { { "<leader>tc", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle color hightlight" } },
+    keys = { { "<leader>uh", "<cmd>CccHighlighterToggle<cr>", desc = "Toggle color highlight" } },
   },
 }
