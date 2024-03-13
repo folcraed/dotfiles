3f2dcde72e449b8f3f6f803b3de7eaa918a34b5b
folcred <robttheb@gmail.com>
2024-03-12 21:37:32 -0600

    nvim: Changed colorscheme
    Giving tokyonight a try for a while, did some mods to make it more
    to my liking.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "onedark",
-        },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
     },
+  },
 }
