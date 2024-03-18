ceb002705271a28806034623da526fc6a79ab5b7
folcred <robttheb@gmail.com>
2024-03-18 09:02:26 -0600

    emacs: Changed theme again back to dark
    While light themes are more readable for me, after some time they
    tend to get hard on my eyes, so going back to dark.

--- init.el
+++ init.el
@@ -475,10 +475,10 @@
 (defun sort-words (reverse beg end)
   "Sort words in region alphabetically, in REVERSE if negative.
     Prefixed with negative \\[universal-argument], sorts in reverse.
-  
+
     The variable `sort-fold-case' determines whether alphabetic case
     affects the sort order.
-  
+
     See `sort-regexp-fields'."
   (interactive "*P\nr")
   (sort-regexp-fields reverse "\\w+" "\\&" beg end))
@@ -492,7 +492,7 @@
 ;; Custom settings
 ;; ==============================================
 (setq custom-safe-themes t)
-(load-theme 'doom-one-light t)
+(load-theme 'doom-one t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
