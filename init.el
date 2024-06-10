53ab513980fe552c48ee2a6c27b8a14fc67db9de
folcred <robttheb@gmail.com>
2024-06-10 07:52:19 -0600

    emacs: Added GFM export package
    When exporting org files, the ox-gfm package creates normal tables
    instead of the HTML ones normal export does.

--- init.el
+++ init.el
@@ -82,6 +82,13 @@
 	(org-agenda-files :maxlevel . 2)))
 
 ;; ==============================================
+;; Org export to Github flavored markdown
+;; ==============================================
+(use-package ox-gfm)
+(eval-after-load "org"
+  '(require 'ox-gfm nil t))
+
+;; ==============================================
 ;; Langtool grammar checking for Org files
 ;; ==============================================
 (use-package langtool)
@@ -514,7 +521,7 @@
  '(cursor-type '(bar . 2))
  '(org-export-backends '(ascii html md odt))
  '(package-selected-packages
-   '(langtool all-the-icons vertico consult orderless marginalia corfu cape project markdown-mode flyspell-correct rg winum which-key use-package tablist rainbow-mode rainbow-delimiters org-superstar minions magit iedit expand-region doom-themes doom-modeline avy transpose-frame async))
+   '(ox-gfm langtool all-the-icons vertico consult orderless marginalia corfu cape project markdown-mode flyspell-correct rg winum which-key use-package tablist rainbow-mode rainbow-delimiters org-superstar minions magit iedit expand-region doom-themes doom-modeline avy transpose-frame async))
  '(project-vc-extra-root-markers '("project.txt")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
