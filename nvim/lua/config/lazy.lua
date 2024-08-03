0fab2a17313deb42b388e924ed5577c4856e9a4a
folcred <robttheb@gmail.com>
2024-08-03 12:06:23 -0600

    nvim: Trying out ruff for linting/formatting
    A lot of devs lately have been switching to ruff for python linting and
    formatting, so going to try it out, see what the draw is.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,7 +14,6 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
--- nvim/lua/plugins/linters.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      python = { "flake8" },
-    },
-  },
-}
