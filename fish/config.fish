e4b549cba4f3f65bf0e107a980b988edfe5c2b64
folcred <robttheb@gmail.com>
2024-12-29 16:50:19 -0700

    fish: Function to restart non-systemd Emacs daemon
    I had one that restarts the systemd one, not one that I start
    from the CLI.

--- fish/config.fish
+++ fish/config.fish
@@ -165,8 +165,14 @@ function rgs --description "Searches current folder and shows 2 lines with match
     rg -C 2 --max-depth 1 $argv
 end
 
-function rsemacs --description "Restarts the emacs daemon"
+function rsemacs --description "Restarts the emacs systemd daemon"
     systemctl --user restart emacs
+end
+
+function remacs --description "Restarts the emacs daemon"
+    killall emacs
+    sleep 2
+    emacs --bg-daemon
 end
 
 function ytdl
