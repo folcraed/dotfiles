946d364c7ff49de6081c0177f9077b8f655801cd
folcred <robttheb@gmail.com>
2025-07-05 09:56:23 -0600

    Modified tab complete to work around new upstream bug
    Since LazyVim's author and lead folke is MIA since 12 May, and
    recent update in blink introduced a bug in tab complete setting,
    had to search for a workaround. This fixed it.

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -8,6 +8,11 @@ return {
     opts = {
       keymap = {
         preset = "super-tab",
+        ["<Tab>"] = {
+          require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+          require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        },
       },
     },
   },
