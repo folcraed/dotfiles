e37dc6ef6dbf902990896762ac032abdb2ed7b8d
folcred <robttheb@gmail.com>
2025-12-29 16:14:13 -0700

    Changes from fish shell update
    There were some changes in the latest version
    of fish that were automatically done when the
    new version was started.

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,38 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 3b4252
+set --global fish_color_cancel normal
+set --global fish_color_command 98c379
+set --global fish_color_comment 616e88
+set --global fish_color_cwd 98c379
+set --global fish_color_cwd_root e06c75
+set --global fish_color_end c678dd
+set --global fish_color_error be5046
+set --global fish_color_escape 56b6c2
+set --global fish_color_history_current normal
+set --global fish_color_host normal
+set --global fish_color_host_remote d19a66
+set --global fish_color_match normal
+set --global fish_color_normal normal
+set --global fish_color_operator 5e81ac
+set --global fish_color_param 61afef
+set --global fish_color_quote a3be8c
+set --global fish_color_redirection e5c07b
+set --global fish_color_search_match e5c07b
+set --global fish_color_selection abb2bf
+set --global fish_color_status e06c75
+set --global fish_color_user 98c379
+set --global fish_color_valid_path normal
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description e5c07b
+set --global fish_pager_color_prefix abb2bf
+set --global fish_pager_color_progress 5e81ac
+set --global fish_pager_color_selected_background -r
